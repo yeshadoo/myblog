5f3499cac7256d8ab9ab7adc1303b412feb475a7
yeshadoo <hc15024839064@gmail.com>
2020-10-03 09:52:05 +0800

    修改包名

--- src/test/java/com/hadoo/service/RedisTest.java
+++ src/test/java/com/hadoo/service/RedisTest.java
@@ -1,18 +1,12 @@
 package com.hadoo.service;
 
-import com.hadoo.mapper.CategoryMapper;
-import com.hadoo.mapper.FriendLinkMapper;
 import com.hadoo.mapper.VisitorMapper;
-import com.hadoo.model.FriendLink;
 import com.hadoo.redis.HashRedisServiceImpl;
-import com.hadoo.redis.StringRedisServiceImpl;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import java.util.LinkedHashMap;
 
 /**
  * @author: zhangocean
